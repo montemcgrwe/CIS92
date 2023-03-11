463f7b6b612ddf8f04a2b11e1df7f68ca358cb5c
montemcgrwe <monte.mcgrew@gmail.com>
2023-03-11 03:36:29 +0000

    changes to Dockerfile RUN -e PORT=8080, EXPOSE

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ RUN pip install django
 COPY mysite /mysite 
 WORKDIR /mysite
 #--------------------
-#RUN -e PORT=8080
-#EXPOSE 8080o
+RUN -e PORT=8080
+#EXPOSE 8080
 EXPOSE $PORT
 #--------------
 CMD python3 ./manage.py runserver 0.0.0.0:8000
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!=g2nd=gn10hbb26@^_3oow(9j1e0*^_a*m%+39%7(9del$%e'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [ "*" ]
+CSRF_TRUSTED_ORIGINS = [ "https://*.run.app" ]
 
 # Application definition
 
